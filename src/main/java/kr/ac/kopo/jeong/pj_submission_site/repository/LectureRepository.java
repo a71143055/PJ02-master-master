463cf6f18697aa19d5114c98879be01d02e95186
a71143055 <a71143055@gmail.com>
2025-09-25 20:53:35 +0900

    PJ02-master-master

--- src/main/java/kr/ac/kopo/jeong/pj_submission_site/repository/LectureRepository.java
+++ src/main/java/kr/ac/kopo/jeong/pj_submission_site/repository/LectureRepository.java
@@ -1,10 +1,11 @@
 package kr.ac.kopo.jeong.pj_submission_site.repository;
 
 import kr.ac.kopo.jeong.pj_submission_site.model.Lecture;
+import kr.ac.kopo.jeong.pj_submission_site.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LectureRepository extends JpaRepository<Lecture, Long> {
-    List<Lecture> findByProfessorId(String professorId);
+    List<Lecture> findByProfessor(User professor);
 }
